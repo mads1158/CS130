3daa08d9f956a79705a5eb253242a61dd8abcc8c
mads1158 <mads1158@bears.unco.edu>
2024-10-14 11:14:30 -0600

    Add files via upload

---
+++ Activity 1/Activity_1A.py
@@ -0,0 +1,8 @@
+gravity = 5 #value of gravity
+velocity = 2 #initial velocity
+time = 10 #time it took to fall
+distance = velocity * time + ((1/2) * gravity * time**2) #position formula
+print(distance)
+#for 25 seconds: 3062.5
+#for initial velocity 2: 510.00000000000006
+#for gravity of 5: 270.0
---
+++ Activity 1/Activity_1B.py
@@ -0,0 +1,16 @@
+my_name = 'Jackson'
+my_major = "Mathematics"
+welcome = my_name + ', welcome to Python!'
+my_lower_name = my_name.lower()
+my_upper_major = my_major.upper()
+my_course = 'CS 130'
+my_capital_course = my_course.capitalize()
+print(welcome)
+print(welcome*3)
+print(my_major + ' is the best major')
+print(my_name + ' is an exceptional student of the ' + my_major + ' program')
+print(my_lower_name)
+print(my_upper_major)
+print(my_lower_name + ' would be written like this in lowercase')
+print('I am taking the course: ' + my_capital_course)
+
